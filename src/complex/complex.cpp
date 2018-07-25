6333e78c1850c4b0114d04951d2eef3b3e2dac66
Daniel Katz <danielkatz@Daniels-MacBook-Pro.local>
2018-07-25 14:07:17 +0200

    Exercise 3.2

--- src/complex/complex.cpp
+++ src/complex/complex.cpp
@@ -1,24 +1,20 @@
 // complex.cpp
 // Multiplying complex numbers
 
-#include <iostream>	// contains the definition of cout, endl
-#include <complex>	// contains the complex class definition
-
+#include <iostream> // contains the definition of cout, endl
+#include <complex> // contains the complex class definition
 using namespace std; // cout, endl, complex are all part of this namespace
+typedef complex<float> fcomp; // initializing an alias for complex<float>
 
 int main()
 {
-	auto num1 = complex<float>{2.0, 2.0};  // use auto for type deduction
-	auto num2 = complex<float>{4.0, -2.0}; // use uniform initialisation syntax (curly braces)
-
-	auto answer = num1 * num2; // type deduced for 'answer' is: complex<float>
-
-	cout << "The answer is: " << answer << endl;
-	cout << "Or in a more familiar form: " << answer.real() 
-			<< " + " << answer.imag() << "j" 
-			<< endl	<< endl;
+    fcomp num1(2.0, 2.0); // use auto for type deduction
+    fcomp num2(4.0, -2.0); // use uniform initialisation syntax (curly braces)
+    auto answer = num1 * num2; // auto deduces a type for 'answer'
+    cout << "The answer is: " << answer<< endl;
+    cout << "Or in a more familiar form: " << answer.real()<< " + " <<answer.imag() << "j"<< endl << endl;
 
 	// answer++; // The program is unable to to increment a value of type complex<float>.
-                 // It does not make sense to increment a complex type as the increment is not defined as real or imaginary. 
+                 // It does not make sense to increment a complex type as the increment is not defined as real or imaginary.
 	return 0;
 }
